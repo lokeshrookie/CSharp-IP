a1edb924b7ee6bb2cbfced72b739b1c505d2ea7d
lokeshrookie <rocklinglokesh@gmail.com>
2024-06-15 19:59:28 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_Sharp_Turorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
