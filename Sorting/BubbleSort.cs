934af700c7fd73ac31c38ee4e8c888c4fedf2b6b
lokeshrookie <rocklinglokesh@gmail.com>
2024-06-15 22:29:40 +0530

    bubblesort

--- Sorting/BubbleSort.cs
+++ Sorting/BubbleSort.cs
@@ -9,8 +9,9 @@ namespace C_Sharp_Turorial.Sorting
 {
 
     /**
-     * 1. Outer loop iterates all elements 
-     * 2. inner loop compares 
+     * 1. Outer loop iterates n times. 
+     * 2. Inner loop starts from second(1st index) element. 
+     * 3. For every index, it compares its previous element. If prev > current, swap both.
      */
     internal class BubbleSort
     {
@@ -18,7 +19,8 @@ namespace C_Sharp_Turorial.Sorting
         {
             int[] arr = { 5, 4, 3, 2, 1 };
             Console.WriteLine( string.Join(" ", arr));
-            Bubble(arr);
+            //Bubble(arr);
+            ShortBubble(arr);
             Console.WriteLine(string.Join(" ", arr));
 
 
@@ -33,6 +35,24 @@ namespace C_Sharp_Turorial.Sorting
                     if (arr[i] > arr[j])
                     {
                         Swap(arr, i, j);
+                    }
+                }
+            }
+        }
+
+        /** Short Bubble Optimization:
+         * This optimization leverages the fact that with each pass, the largest element bubbles to the end.
+         * So, we can progressively reduce the inner loop's upper bound in subsequent passes. Here's the modified code:
+         */
+        public static void ShortBubble(int[] arr)
+        {
+            for(int i = 0; i<arr.Length-1; i++)
+            {
+                for(int j = 0; j<arr.Length-1-i; j++) // Upper bound reduced by i+1 in each pass
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        Swap(arr, j, j + 1);
                     }
                 }
             }
