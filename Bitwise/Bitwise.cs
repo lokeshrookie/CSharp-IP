621f3008dc45c96b3589b3be966e7405fc87b480
lokeshrookie <rocklinglokesh@gmail.com>
2024-06-16 18:04:43 +0530

    Unique

--- Bitwise/Bitwise.cs
+++ Bitwise/Bitwise.cs
@@ -8,7 +8,24 @@ namespace C_Sharp_Turorial.Bitwise
 {
     public class Bitwise
     {
+        // Find Unique (non repeating number) in an arary
+        public static int Unique(int[] arr)
+        {
+            int unique = 0;
 
+            for(int i = 0; i<arr.Length; i++)
+            {
+                Console.WriteLine("Before " + i + " " + unique);
+                unique ^= arr[i];
+                Console.WriteLine("After " + i + " " + unique);
+
+            }
+
+            return unique;
+        }
+
+
+        // returns true if n is odd. Else, false will be returned.
         public static bool IsOdd(int n)
         {
             if((n&1) == 1)
