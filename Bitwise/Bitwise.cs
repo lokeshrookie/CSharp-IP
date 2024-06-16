ef6ede8e5886625da1ab142d7fea1dd535ebe928
lokeshrookie <rocklinglokesh@gmail.com>
2024-06-16 17:59:36 +0530

    IsOdd

---
+++ Bitwise/Bitwise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Turorial.Bitwise
+{
+    public class Bitwise
+    {
+
+        public static bool IsOdd(int n)
+        {
+            if((n&1) == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+}
