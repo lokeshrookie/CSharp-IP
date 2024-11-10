11223e4f614c9bdf398972b8f6868021228e555a
lokeshrookie <rocklinglokesh@gmail.com>
2024-11-10 23:07:22 +0530

    strings

---
+++ Strings/String.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Turorial.Strings
+{
+    public   class String
+    {
+        public static string ReverseWords(string s)
+        {
+            string[] words = s.Split(' ');
+            Array.Reverse(words);
+            return string.Join(" ", words);
+
+         
+        }
+    }
+
+
+}
