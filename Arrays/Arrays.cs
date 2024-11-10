4bb0fb8af63165b7f1b79a785062a554cb508c4d
lokeshrookie <rocklinglokesh@gmail.com>
2024-11-10 23:06:51 +0530

    Arrays

--- Arrays/Arrays.cs
+++ Arrays/Arrays.cs
@@ -8,28 +8,6 @@ namespace C_Sharp_Turorial.Arrays
 {
     internal class Arrays
     {
-        public static void Main(string[] args)
-        {
-           int[] arr = { 5, 4, 3, 2, 1 };
-
-           // arr = arr.OrderBy(x=> x);
-            // this won't work becuase IEnumerable cant cast to Array type.
-            
-           Console.WriteLine(string.Join(",", arr.OrderBy(x => x)) );
-           IEnumerable<int> arr2 = arr.OrderBy(s => s);
-
-           Console.WriteLine(string.Join(",", arr2));
-
-        //    // GET - based on Index.
-        //    Console.WriteLine(arr[0]);
-        //    Console.WriteLine(arr.GetValue(0));
-
-        //    // GET - All Array elements
-        //    Console.WriteLine(string.Join(" ", arr)); // space(" ") is delimiter between array elements.
-
-        //    // GET All - by using LINQ
-        //    Console.WriteLine(string.Join(" ", arr.Select(x => x)));
-
-        }
+      
     }
 }
