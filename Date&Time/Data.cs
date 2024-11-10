ac509d70b8e9d0d49d47559c1e06ccc0724cd2d7
lokeshrookie <rocklinglokesh@gmail.com>
2024-11-10 23:07:05 +0530

    DateTime

---
+++ Date&Time/Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Turorial.Date_Time
+{
+    public class DataAndTime
+    {
+        public static void main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+
+            Console.WriteLine(dt);
+        }
+    }
+}
